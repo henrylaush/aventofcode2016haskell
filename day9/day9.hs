a6f2420e33253e8a4938726f7551e6e5a4fdf503
HenryLau <henrylau@seekasia.com>
2017-03-17 15:10:49 +0800

    Refactor, add state for part 2

--- day9/day9.hs
+++ day9/day9.hs
@@ -5,10 +5,7 @@ import Data.List (sum)
 
 data Part = R Char | M Int Int deriving (Show, Eq)
 data FState = A Int | D Int Int deriving (Show, Eq)
-
-partLength :: Part -> Int
-partLength (R _) = 1
-partLength (M a b) = 3 + length (show a) + length (show b)
+data P2State = D2 Int [(Int, Int)] deriving (Show, Eq)
 
 parseInt :: Parser Int
 parseInt = read <$> many digit
@@ -19,11 +16,19 @@ parsePart = many (R <$> try upper <|> try parseMarker)
 parseMarker :: Parser Part
 parseMarker = between (char '(') (char ')') (M <$> parseInt <* char 'x' <*> parseInt)
 
+parseInput :: [String] -> [[Part]]
+parseInput = rights . map (parse parsePart "")
+
+-- Part 1
+partLength :: Part -> Int
+partLength (R _) = 1
+partLength (M a b) = 3 + length (show a) + length (show b)
+
 expand :: FState -> Part -> FState
 expand (A t) (R _) = A (t + 1)
 expand (A t) (M a b) = D (t + a * b) a
 expand (D t td) (R _) | td == 1 = A t | otherwise = D t (td - 1)
-expand (D t td) m@(M _ _) 
+expand (D t td) m@(M _ _)
     | ml < td = D t (td - ml)
     | otherwise = A (t + ml - td)
     where ml = partLength m
@@ -32,16 +37,15 @@ getLength :: FState -> Int
 getLength (A a) = a
 getLength D{} = error "Still dropping"
 
--- Part 1
 part1 :: [String] -> Int
-part1 = sum . map (getLength . foldl expand (A 0)) . rights . map (parse parsePart "")
+part1 = sum . map (getLength . foldl expand (A 0)) . parseInput
 
 -- Part 2
 part2 :: [String] -> Int
 part2 = undefined
 
 main :: IO()
-main = readInput >>= \content -> 
+main = readInput >>= \content ->
        print (part1 content) >>
        print (part2 content) >>
        return ()
@@ -51,7 +55,7 @@ readInput = lines <$> readFile "input"
 
 testCase :: [String]
 testCase = ["ADVENT", -- 6
-            "A(1x5)BC", -- 7 
+            "A(1x5)BC", -- 7
             "(3x3)XYZ", -- 9
             "A(2x2)BCD(2x2)EFG", -- 11
             "(6x1)(1x3)A", -- 6
@@ -59,6 +63,6 @@ testCase = ["ADVENT", -- 6
 
 testCase2 :: [String]
 testCase2 = ["(3x3)XYZ", -- 9
-             "X(8x2)(3x3)ABCY", -- 20 
+             "X(8x2)(3x3)ABCY", -- 20
              "(27x12)(20x12)(13x14)(7x10)(1x12)A", -- 241920
              "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"] -- 445
